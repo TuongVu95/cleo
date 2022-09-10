029074bb40251dd5fe94fa43da7f795e1471f534
26960 <vunv20@fpt.com.vn>
2022-09-10 11:32:23 +0700

    update

--- js/function.js
+++ js/function.js
@@ -1,13 +1,34 @@
-var swiper = new Swiper(".slide", {
-    slidesPerView: 4,
-    spaceBetween: 32,
-    slidesPerGroup: 4,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-  });
+const fnHome = () => {
+    if($('.slide').length){
+    var swiper = new Swiper(".slide", {
+        slidesPerView: 4,
+        spaceBetween: 32,
+        slidesPerGroup: 4,
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+      });
+    }
+}
+fnHome();
+
+
+const introduce = (params) => {
+    if($('.js-banner').length){
+        var swiper = new Swiper(".js-banner", {
+            slidesPerView: 1,
+            spaceBetween: 0,
+            pagination: {
+              el: ".swiper-pagination",
+              clickable: true,
+            },
+          });
+    }
+
+}
+introduce();
