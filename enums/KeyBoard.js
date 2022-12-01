674c10b98a151abc5d3873839022ac6b30976f96
Vu <neulvu95@gmail.com>
2022-12-01 22:25:52 +0700

    update

--- enums/KeyBoard.js
+++
@@ -1,9 +0,0 @@
-const KeyboardEventCode = {
-  Enter : 'Enter',
-  Escape : 'Escape',
-  ArrowDown : 'ArrowDown',
-  ArrowLeft : 'ArrowLeft',
-  ArrowUp : 'ArrowUp',
-  ArrowRight : 'ArrowRight',
-  BackSpace : 'BackSpace'
-}
